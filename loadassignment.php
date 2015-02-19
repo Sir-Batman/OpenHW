37f1a45afde72dbd067469cc3541ee6b4aa85713
Connor Yates <yatesco@flip2.engr.oregonstate.edu>
2015-02-19 15:33:32 -0800

    Initial commit, adding work so far

---
+++ loadassignment.php
@@ -0,0 +1,96 @@
+<?php
+header("content-type:application/json");
+
+$dbinfo = parse_ini_file("database.ini");
+$db = new mysqli($dbinfo['server'],
+			$dbinfo['username'], 
+			$dbinfo['password'], 
+			$dbinfo['database']);
+
+		if( !$db || $db->connect_errno) {
+			echo "There was an error connecting to the database: ".$db->connect_errno." ; ".$db->connect_error;
+		}
+		else{
+			//echo "*Hacker voice* 'We're in'<BR>";
+		}
+
+//will take in a user, and see if they are in the database
+/*$uname = "'{$_POST['uname']}'";
+$query = "select userName from students";
+$result = $db->query($query);
+
+if($result->num_rows > 0){
+	echo $uname;
+	while($row = $result->fetch_assoc()){
+		echo "ROW: ".$row["userName"]."<br>";
+		//Add "'" to the from and end of the username to make it match the value
+		//that we get from POST
+		if ($uname === "'".$row["userName"]."'" ){
+			echo "Found user!";
+			$ass = "select * from assignments where ass_id=1";
+			$questions
+		}
+		else{
+			echo "User not found";
+		}
+	}
+}
+else{
+	echo "There are no users!<br>";
+}
+ */
+
+//here, we will be just grabbing some values from the database, hardcoded, so to get this thing working
+//ideally, this will return a json, which index.html can load up and use to populate the page.
+
+
+$return_array = array();
+$ass = $db->query("select * from assignments where ass_id=1");
+if($ass->num_rows > 0){
+	while($row = $ass->fetch_assoc()){
+		//var_dump($row);
+		//echo $row["ass_id"]."<br>";
+		$row_arr['name'] = $row['ass_t'];
+	}
+}
+else{
+	echo "Ass is 0<br>";
+}
+
+
+$q = $db->query("select * from questions where ass_id=1");
+
+if($q->num_rows > 0){
+	$row_arr['questions'] = array();
+
+	while($row = $q->fetch_assoc()){
+		
+		$sol = $db->query("select * from solutions where q_id='{$row['q_id']}'");
+		$q_arr['questionname'] = "Question ".$row['q_id'];
+		$q_arr['question']=$row['q_t'];
+		//array_push($return_array, $row['q_t']);
+
+		$sol_arr = array();
+		while($row_s = $sol->fetch_assoc()){
+
+			array_push($sol_arr, $row_s['sol_t']);
+		}
+		$q_arr['answers']=$sol_arr;
+		//THIS IS HARDCODED
+		$q_arr['type']="multiplechoice";
+		array_push($row_arr['questions'], $q_arr);
+	}
+
+	//the last push
+	$return_array['assignment'] = $row_arr;
+}
+else{
+	echo "q is zero<br>";
+}
+
+//echo "return array!!!";
+//echo "return array: ".$return_array;
+echo json_encode($return_array);
+
+mysqli_close($db);
+?>
---
+++ myscripts.js
@@ -0,0 +1,115 @@
+
+//nojs:
+//if javascript is allowed on the page, hides the 
+//"Y NO JS, BUB?" message
+function nojs(){
+	$(".nojs").addClass("invisible");
+}
+
+
+//multipleChoice:
+//Creates the multiple choice section for the question, and returns the
+//jquery form object populated with the proper data
+function multipleChoice(question){
+	
+	var answers = $("<div></div>").addClass("answers");
+	var form = $("<form></form>").addClass("multipleChoice");
+	
+	for(var j = 0; j < question.answers.length; j++){
+		var ans = $('<input type="radio" name=current.questionname value=current.naswers[j] id=current.questionname>');
+		ans.appendTo(form).after($("<br>")).after(question.answers[j]);
+		//$("<br>").appendTo(answers);	
+	}
+	
+	form.appendTo(answers);
+	
+	return answers;
+}
+
+
+//loadQuestion:
+//takes in a question object, and creates and populates a div for 
+//the single question
+//returns a jquery div 
+function loadQuestion(question) {
+	//alert(current.question);
+	var wrapper = $("<div></div>").addClass("question");
+	var header = $("<h1></h1>").text(question.questionname);
+	var quest = $("<p></p>").text(question.question);
+	
+	//This section creates the answer/input section, based on the question type
+	if(question.type == "multiplechoice"){
+		var answers = multipleChoice(question);		
+	}
+	
+	else{
+		console.log("UNSUPORTED QUESTION TYPE");
+	}
+	
+	var submit = $("<button type='submit'></button>").text("Submit");
+	submit.addClass("submitbutton");
+	var submitdiv = $("<div></div>").addClass("submitbuttondiv");
+	
+	submit.appendTo(submitdiv);					
+	header.appendTo(wrapper);
+	quest.appendTo(wrapper);
+	answers.appendTo(wrapper);
+	submitdiv.appendTo(wrapper);
+	
+	return wrapper;	
+}
+
+//loadAssignment:
+//loads the assignment data from the static json located in the same 
+//directory
+function loadAssignment(d) {
+	//Add the assignment specific data
+	var addtopage = $(".assignment");	
+	
+	//console.log("in loadquestions");
+	//console.log("Assignment name: " + d.assignment.name);
+	
+	var assname = $("<h1></h1>").text(d.assignment.name).addClass("assignmentName");
+	assname.appendTo(addtopage);
+	
+	//load the questions
+	for(var i = 0; i < d.assignment.questions.length; i++) {
+		var current = d.assignment.questions[i];
+		var wrapper = loadQuestion(current);
+		
+		wrapper.appendTo(addtopage);		
+	}
+}
+
+
+//straightforward ajax call to load from the json file
+function loadData(){
+	$.ajax({
+		url:"./loadassgnment.php",
+		dataType:"json",
+		success:function (d) {
+			//console.dir(d);
+			loadAssignment(d);
+		},
+		error:console.log("Error loading json")
+	});
+}
+			
+
+$(document).ready(function (){
+	console.log("document ready");
+	
+	//firefox json fix
+	$.ajaxSetup({'beforeSend': function(xhr){
+		if (xhr.overrideMimeType)
+			xhr.overrideMimeType("text/plain");
+		}
+	});
+	
+	//remove the nojs warning
+	nojs();
+	
+	//load the assignment data
+	loadData();
+	
+});
