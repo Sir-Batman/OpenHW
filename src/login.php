a9c10e9d946cd0f2585fb94721a0f484431a61d4
Connor Yates <yatesco@flip2.engr.oregonstate.edu>
2015-02-25 00:29:48 -0800

    Create basic login functionality, fixes #1

---
+++ src/login.php
@@ -0,0 +1,45 @@
+<?php
+session_start();
+
+//Connect to the database
+$dbinfo = parse_ini_file("../database.ini");
+$db = new mysqli($dbinfo['server'],
+			$dbinfo['username'], 
+			$dbinfo['password'], 
+			$dbinfo['database']);
+
+if( !$db || $db->connect_errno) {
+	echo "There was an error connecting to the database: ".$db->connect_errno." ; ".$db->connect_error;
+}
+else{
+	//echo "*Hacker voice* 'We're in'<BR>";
+}
+
+$query = $db->prepare("SELECT * FROM students WHERE username = ?");
+$query->bind_param("s", $username);
+
+$username = $_POST['username'];
+$query->execute();
+
+$result = $query->get_result();
+
+$row = $result->fetch_assoc();
+//echo '<br>QUERY: ', $row['firstname'], ' ', $row['lastname'];
+
+if( $row['password'] === $_POST['password'] ) {
+
+	//Log in via sessions
+	$_SESSION['last_activity'] = 1;
+
+	//echo "You are being logged in...";
+	echo "<br>session: ", $_SESSION['last_activity'];
+}
+else{
+	echo "Invalid!!!";
+}
+
+?>
+
+
+
+
